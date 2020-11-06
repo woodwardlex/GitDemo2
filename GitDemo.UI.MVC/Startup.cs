8cda78e06940518097e256466206f4fc0e28c253
Alexis Woodward <alexis.woodward1717@outlook.com>
2020-11-06 11:20:24 -0600

    Initial commit

---
+++ GitDemo.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDemo.UI.MVC.Startup))]
+namespace GitDemo.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
